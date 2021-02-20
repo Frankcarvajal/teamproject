78e9f3a66a56aa2ef39724fe5f93bb5d5cb71936
frankcarvajal <Frankacarv@gmail.com>
2021-02-20 00:56:04 -0800

    first bit of changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Fetch from "./Fetch";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">Hello World</header> */}
       <Fetch />
     </div>
   );
